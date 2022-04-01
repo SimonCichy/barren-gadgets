8bfa322f607a9c541e7ab645b92134a5a9d01b60
Simon Cichy <simon.cichy@outlook.com>
2022-04-01 18:10:35 +0200

    Update the plotting script
    Updated details like ticks at integers and y limits

--- plot_variances.py
+++ plot_variances.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 colours = ['#6f45d8', '#4547d8', '#45ccd8', '#45d859', '#c2d845', '#d89d45', '#d85645']
 
-file = '../results/data/global_circuit_4qubits_5layers_200samples.dat'
+file = '../results/data/220401_global_circuit_6qubits_50layers_1000samples.dat'
 
 data = np.loadtxt(file)
 
 layers = data[:,0].astype(int)
 qubits = data[:,1].astype(int)
 gradients = data[:,2:]
+
+fig, ax = plt.subplots()
 
 # getting the different numbers of layers tested
 layers_list = np.unique(layers)
@@ -21,9 +24,11 @@ for nl, num_layers in enumerate(layers_list):
     qubits_list = qubits[row_indexes]
     variance_vals = np.var(grad_vals, axis=1)
     
-    plt.semilogy(qubits_list, variance_vals, "--o", c=colours[nl], label="{} layers".format(num_layers))
+    ax.semilogy(qubits_list, variance_vals, "--o", c=colours[nl], label="{} layers".format(num_layers))
 
-plt.xlabel(r"N Qubits")
-plt.ylabel(r"$\langle \partial \theta_{1, 1} E\rangle$ variance")
-plt.legend()
+# ax.set_ylim([2.5e-3, 1.25e-1])
+ax.set_xlabel(r"N Qubits")
+ax.set_ylabel(r"$\langle \partial \theta_{1, 1} E\rangle$ variance")
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.legend()
 plt.show()
