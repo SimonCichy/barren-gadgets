a2cf26c002c1b1f04e766e884af2dd4169f85d93
Simon Cichy <simon.cichy@outlook.com>
2022-04-13 14:09:49 +0200

    Moving HEA
    Moving the hardware efficient ansatz and cat state preparation to an own file

--- own-experiment-classes/gadget_cost.py
+++ own-experiment-classes/gadget_cost.py
@@ -2,53 +2,7 @@
 import pennylane as qml
 from pennylane import numpy as np
 
-
-def hardware_efficient_ansatz(params, gate_sequence=None, rot_y = True):
-    """A random variational quantum circuit based on the hardware efficient ansatz. 
-    There are no measurements and it is to be used within the global or local circuits
-
-    Args:
-        params (array[array[float]]): array of parameters of dimension (num_layers, num_qubits) containing the rotation angles
-
-    Returns:
-        not sure... see pennylane documentation
-    """
-
-    # Relevant parameters
-    assert(len(np.shape(params)) == 2)      # check proper dimensions of params
-    num_layers = np.shape(params)[0]        # np.shape(params) = (num_layers, num_qubits)
-    num_qubits = np.shape(params)[1]
-    
-    if gate_sequence == None:
-        # Generating the gate sequence from randomly applying RX, RY or RZ with the corresponding rotation angle
-        gate_set = [qml.RX, qml.RY, qml.RZ]
-        random_gate_sequence = [[np.random.choice(gate_set) for _ in range(num_qubits)] for _ in range(num_layers)]
-        gate_sequence = random_gate_sequence
-
-    # Initial rotations on all qubits
-    if rot_y:
-        for i in range(num_qubits):             # rotate all qubits
-            qml.RY(np.pi / 4, wires=i)          # "to prevent X, Y , or Z from being an especially preferential 
-                                                # direction with respect to gradients."
-
-    # Repeating a layer structure
-    for l in range(num_layers):
-        # Single random gate layer (single qubit rotations)
-        for i in range(num_qubits):
-            gate_sequence[l][i](params[l][i], wires=i)
-        # Nearest neighbour controlled phase gates
-        if num_qubits > 1:                          # no entangling gates if using a single qubit
-            qml.broadcast(qml.CZ, wires=range(num_qubits), pattern="ring")
-
-
-def cat_state_preparation(ancillary_qubits):
-    # leave the computational qubits in the |0> state
-    # create a cat state |+> in the ancillary register
-    # /!\ inefficient in depth
-    qml.Hadamard(wires=ancillary_qubits[0])
-    for qubit in ancillary_qubits[1:]:
-        qml.CNOT(wires=[ancillary_qubits[0], qubit])
-
+from gadget_circuits import hardware_efficient_ansatz
 
 class GadgetCost:
     def __init__(self, computational_qubits, total_qubits, device):
@@ -64,7 +18,7 @@ class GadgetCost:
         # qml.PauliX(wires=[0])                       # cheating by preparing the |10...0> state
         # if self.n_tot > self.n_comp:
         #     cat_state_preparation(ancillary_qubits = range(self.n_comp, self.n_tot, 1))
-        hardware_efficient_ansatz(params, gate_sequence, rot_y=False)
+        hardware_efficient_ansatz(params, range(self.n_tot), gate_sequence=gate_sequence, rot_y=False)
         return qml.expval(observable)
     
     def cost_function(self, params, gate_sequence, observable):
