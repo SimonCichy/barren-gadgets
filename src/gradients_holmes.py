2d86663d3ab93bce26ec309873dcc77ea314734c
Simon Cichy <simon.cichy@outlook.com>
2022-04-26 09:39:53 +0200

    Commenting
    Adding descriptions to the GradientHolmes class for better documentation and shareability

--- src/gradients_holmes.py
+++ src/gradients_holmes.py
@@ -7,7 +7,27 @@ from hardware_efficient_ansatz import HardwareEfficientAnsatz
 
 
 class GradientsHolmes:
-    def __init__(self, qubits_list, layer_list = ['linear'], perturbation_factor=1, num_samples=200, gate_set=[qml.RX, qml.RY, qml.RZ]):
+    """ Class containing methods to generate the data to produce the variance
+    plots used in the identification of Barren plateaus (variance of the 
+    gradients of the cost function as a function of depth in number of qubits)
+    
+    Args: 
+        qubits_list (list)  : list of number of qubits to probe the system on
+                              to get one point in the plot
+        layer_list (list or string) : one of the following
+            · list of the number of layers to probe for each number of qubits
+            · string 'linear' to inform to use a circuit depth linear in the 
+              number of qubits
+        perturbation_factor (float) : parameter controlling the magnitude of the
+                                      perturbation in the derivation from 
+                                      Jordan2012 (as prefactor to λmax)
+        num_samples (int)           : number of samples to take to generate the
+                                      statistics of the variance of the gradients
+        gate_set (list)             : set of gates to be used in the ansatz
+    """
+    def __init__(self, qubits_list, layer_list = ['linear'], 
+                 perturbation_factor=1, num_samples=200, 
+                 gate_set=[qml.RX, qml.RY, qml.RZ]):
         self.qubit_list = qubits_list
         self.layer_list = layer_list
         self.gate_set = gate_set
@@ -16,12 +36,25 @@ class GradientsHolmes:
         self.data_folder = '../results/data/gradients/'
     
     def generate(self, circuit):
+        """Wrapper function to generate the gradient data in function of the 
+        kind of layering. For more details, see called functions: 
+        · generate_linear_depth_gradients() 
+        · generate_fixed_depth_gradients
+        Args: 
+            circuit (string): identifier of the circuit to be probed. should be
+                              one of the following: 'global', 'local', 'gadget2',
+                              'gadget3'
+        """
         if self.layer_list == 'linear':
             self.generate_linear_depth_gradients(circuit)
         elif type(self.layer_list[0]) is int:
             self.generate_fixed_depth_gradients(circuit)
 
     def generate_fixed_depth_gradients(self, circuit):
+        """Method generating gradient data for the requested circuit for all 
+        combinations of widths and depths as given by self.qubits and 
+        self.layers
+        """
         file_name = self.get_file_name(circuit)
         with open(file_name, 'w') as of:
             of.write('# layers\t# qubits\tgradients')
@@ -43,6 +76,10 @@ class GradientsHolmes:
             of.write('\n')
     
     def generate_linear_depth_gradients(self, circuit):
+        """Method generating gradient data for the requested circuit for a 
+        fixed depth for each width in self.qubits. It uses as many layers as 
+        total number of qubits (computational + ancillary)
+        """
         file_name = self.get_file_name(circuit)
         with open(file_name, 'w') as of:
             of.write('# layers\t# qubits\tgradients')
@@ -69,9 +106,29 @@ class GradientsHolmes:
             of.write('\n')
     
     def get_cost(self, circuit, num_qubits, num_layers):
+        """Method to generate the cost function of which the gradients will be
+        recorded.
+        TODO: transform to 'get_gradient(self, circuit, num_qubits, num_layers) 
+        -> float ' instead
+        
+        Args: 
+            circuit (string): identifier of the circuit to be probed. should be
+                              one of the following: 'global', 'local', 'gadget2',
+                              'gadget3'
+            num_qubits (int): number of computational qubits (qubits acted upon  
+                              by the global Hamiltonian)
+            num_layers (int): depth of the circuit to probe
+        Returns: 
+            cost (callable) : callable function to obtain the differentiable
+                              cost value for given parameters (using 
+                              qml.ExpvalCost)
+            params (list)   : list of (randomly generated) parameters to probe
+                              the constructed cost function
+        """
         if 'gadget' in circuit:
+            # finding the target locality of the gadget decomposition
             locality = int(circuit[circuit.find('gadget') + 6])
-            assert num_qubits % (locality - 1) == 0, "Non integer numer of requested ancillary qubits. The number of computational qubits is not divisible by (locality-1)"
+            assert num_qubits % (locality - 1) == 0, "Non integer number of requested ancillary qubits. The number of computational qubits is not divisible by (locality-1)"
             ancillary_qubits = int(num_qubits / (locality - 1))
             width = num_qubits+ancillary_qubits
             oH = ObservablesHolmes(num_qubits, ancillary_qubits, self.perturbation_factor)
