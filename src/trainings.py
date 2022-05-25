33c3e100b21cbc7b9b6ac464a151fc31419865a9
Simon Cichy <simon.cichy@outlook.com>
2022-05-25 14:25:53 +0200

    corrections
    Correcting the number of qubits used when training only on the computational Hamiltonian to use only the computational register

--- src/trainings.py
+++ src/trainings.py
@@ -189,15 +189,15 @@ class SchedulesOfInterest:
     def linear_ala_comp(self, perturbation, optimizer, iterations):
         num_layers = self.n_comp
         random_gate_sequence = [[np.random.choice(self.gate_set) 
-                                for _ in range(self.n_tot)] 
+                                for _ in range(self.n_comp)] 
                                 for _ in range(num_layers)]
         ala = AlternatingLayeredAnsatz(random_gate_sequence)
         initial_weights = np.random.uniform(0, np.pi, 
-                            size=(num_layers, self.n_tot), 
+                            size=(num_layers, self.n_comp), 
                             requires_grad=True)
         oH = ObservablesHolmes(self.n_comp, self.n_anc, perturbation)
         schedule = {
-            'device': self.dev_gad,
+            'device': self.dev_comp,
             'optimizers': [optimizer], 
             'ansaetze': [ala],
             'initial weights': initial_weights, 
