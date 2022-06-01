971e7ca6cfb098eb138bef9bb8fb9230da3eb810
Simon Cichy <simon.cichy@outlook.com>
2022-06-01 18:34:59 +0200

    Minor edits
    Removing superfluous code from the data management

--- src/data_management.py
+++ src/data_management.py
@@ -26,23 +26,19 @@ def save_training(schedule, cost_lists, mode='new file'):
 
 def create_todays_subfolder(data_folder, mode='new file'):
     data_folder += '{}'.format(datetime.datetime.now().strftime("%y%m%d"))
-    try:
-        os.makedirs(data_folder) 
-        if mode == 'new file':
+    if mode == 'new file':
+        try:
+            os.makedirs(data_folder) 
             print("Directory      " , data_folder ,  " created ")
-    except FileExistsError:
-        if mode == 'new file':
+        except FileExistsError:
             print("Directory      " , data_folder ,  " already exists")
     return data_folder + '/'
 
 def create_filename(data_folder, data_type, mode='new file'):
     filename = data_folder + data_type + '_nr' + "%04d"%1 + '.dat'
     while os.path.isfile(filename+'.npz'):
-        # try:
         current_count = int(''.join([s for s in filename if s.isdigit()])[-4:]) + 1
         filename = data_folder + 'training_nr' + "%04d"%current_count + '.dat'
-        # except (TypeError, ValueError):
-        #     filename = filename[0:-4] + '_2.csv'
     if mode == 'overwrite':
         current_count -= 1
         filename = data_folder + 'training_nr' + "%04d"%current_count + '.dat'
