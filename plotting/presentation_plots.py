c019869d350dfc7d754a6a10c4cb2cbec297af36
Simon Cichy <simon.cichy@outlook.com>
2022-08-03 17:23:06 +0200

    Plots for presentations
    Repeating the same plots with different settings for the presentations

--- plotting/presentation_plots.py
+++ plotting/presentation_plots.py
@@ -24,53 +24,6 @@ paper_formatter = rsmf.setup(r"\documentclass[aps,prx,twocolumn,superscriptaddre
 
 def training_plots(): 
     lambdas = [0.1, 1, 10]
-    # file_list = ['220708_euler/training_nr00{}'.format(count) for count in [18, 20, 22]]
-    file_list = ['220713_qmio/training_nr{:0>4}'.format(count) for count in [5, 7, 9]]
-    palette_choice = [palette[0][2], palette[2][0]]
-
-    fig = paper_formatter.figure(aspect_ratio=.3, wide=True)
-    plt.rcParams.update({
-        "pgf.preamble": "\n".join([
-            r"\usepackage{dsfont}", 
-            r"\usepackage{amsmath}", 
-        ])
-    })
-    axs = fig.subplots(1, 3)
-    for f, file in enumerate(file_list):
-        file = data_folder + 'training/' + file
-        costs = get_training_costs(file + '.npz')
-        labels = get_training_labels2(file + '.txt')
-        axs[f].plot(np.ones(len(costs[0])), ':', c='gainsboro')
-        axs[f].plot(-np.ones(len(costs[0])), ':', c='gainsboro')
-        axs[f].plot(np.zeros(len(costs[0])), ':', c='gainsboro')
-        for i in range(1, 3):
-            axs[f].plot(costs[i],'-', c=palette_choice[i-1], label=labels[i])
-        axs[f].set_title(r'$\lambda = {}$'.format(lambdas[f]) + r'$\lambda_{max}$', 
-                         fontsize=9)
-        axs[f].set_ylim(top=2)
-    axs[0].set_ylabel('Cost')
-    axs[1].set_xlabel('Number of iterations')
-    custom_lines = [Line2D([0], [0], color=palette_choice[1-nl], lw=1) for nl in range(2)]
-    # lgd = fig.legend(handles=custom_lines,
-    #                 labels=[r'$\mathrm{Tr} \Big[| \psi(\boldsymbol{\theta})\rangle \! \langle \psi(\boldsymbol{\theta})| \big( H^{comp}\otimes\mathds{1}^{anc} \big) \Big]$', 
-    #                         r'$\mathrm{Tr} \Big[| \psi(\boldsymbol{\theta})\rangle \! \langle \psi(\boldsymbol{\theta})| H^{gad} \Big]$'], 
-    #                 loc='upper right',
-    #                 bbox_to_anchor=(1, 0.2),
-    #                 frameon=False)
-    lgd = axs[2].legend(handles=custom_lines,
-                    labels=[r'$ \big \langle H^\mathrm{gad} \big\rangle_{| \psi(\boldsymbol{\theta})\rangle} $', 
-                            r'$ \big \langle H^\mathrm{comp}\otimes\mathds{1}^{anc} \big\rangle_{| \psi(\boldsymbol{\theta})\rangle} $'], 
-                    handlelength=.8, 
-                    loc='upper right',
-                    bbox_to_anchor=(1, 0.96),
-                    edgecolor='1', 
-                    borderpad=0)
-    plt.tight_layout()
-    plt.savefig(data_folder + '../plots/training_new_gadget/trainings_for_paper.pdf', 
-                bbox_extra_artists = (lgd,), bbox_inches='tight')
-
-def training_plots_with_statistics(): 
-    lambdas = [0.1, 1, 10]
     file_list = {}
     file_list[0.1]  = ['220726_qmio11-1/training_nr{:0>4}'.format(nr) for nr in range(1, 11, 3)]
     file_list[0.1] += ['220726_qmio14/training_nr{:0>4}'.format(nr) for nr in range(1, 11, 3)]
@@ -155,7 +108,7 @@ def training_plots_with_statistics():
                         edgecolor='1', 
                         borderpad=0)
     plt.tight_layout()
-    plt.savefig(data_folder + '../plots/training_new_gadget/trainings_for_paper_with_statst.pdf', 
+    plt.savefig(data_folder + '../plots/training_new_gadget/trainings_for_presentation.png', 
                 bbox_extra_artists = (lgd,), bbox_inches='tight')
 
 def get_vars_for_plot(file, max_qubit=np.inf):
@@ -185,7 +138,7 @@ def variances_plots():
     file_gad3 = data_folder + 'gradients/220725_euler/gradients_nr0002_merge.npz'
     file_gad4 = data_folder + 'gradients/220728_euler/gradients_nr0001_merge.npz'
 
-    fig = paper_formatter.figure(aspect_ratio=.8, wide=False)
+    fig = paper_formatter.figure(aspect_ratio=.7, wide=True)
     plt.rcParams.update({
         "pgf.preamble": "\n".join([
             r"\usepackage{dsfont}", 
@@ -224,13 +177,12 @@ def variances_plots():
     custom_lines += [Line2D([0], [0], c=palette[-1][nl], lw=2) for nl in range(len(layers_list))]
     ax.legend(handles=custom_lines, 
               labels=[r'$H^\mathrm{comp}$', '4-local ' + r'$H^\mathrm{gad}$', '3-local ' + r'$H^\mathrm{gad}$'] + [''] + 
-                    #  [r'$\textcolor{white}{0}$' + '{} layers'.format(num_layers) for num_layers in layers_list[:2]] + 
                     [r'$\ \,$' + '{} layers'.format(num_layers) for num_layers in layers_list[:2]] + 
                      ['{} layers'.format(num_layers) for num_layers in layers_list[2:]], 
-                    # ['{:2} layers'.format(num_layers) for num_layers in layers_list], 
-              loc='lower left',
-              bbox_to_anchor=(0.02, 0.94, 0.92, 0),
-              mode='expand',
+            #   loc='lower left',
+            #   bbox_to_anchor=(0.02, 0.94, 0.92, 0),
+              loc='upper right',
+            #   mode='expand',
               ncol=2, 
               handletextpad=1, 
               handlelength=2, 
@@ -239,9 +191,8 @@ def variances_plots():
     ax.spines.right.set_visible(True)
     ax.spines.top.set_visible(True)
     plt.tight_layout()
-    plt.savefig(data_folder + '../plots/variances_new_gadget/variances_for_paper.pdf')
+    plt.savefig(data_folder + '../plots/variances_new_gadget/variances_for_presentation.png')
 
 if __name__ == "__main__":
     # training_plots()
-    training_plots_with_statistics()
-    # variances_plots()
+    variances_plots()
